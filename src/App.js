709c7ec78c0f8d97e9a540dc6d7fe93b0571e136
Karl Umberto <karl.pongolini@gmail.com>
2025-04-01 17:42:21 +0300

    Add map

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { MapContainer } from 'react-leaflet/MapContainer'
+import { TileLayer } from 'react-leaflet/TileLayer'
+import { useMap } from 'react-leaflet/hooks'
+import "leaflet/dist/leaflet.css";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          karl oli siin
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="box">
+      <MapContainer center={[58.2667, 25.5844]} zoom={13} scrollWheelZoom={false} style={{ height: "50%", width: "50%" }}>
+      <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+</MapContainer>
+</div>
       </header>
     </div>
   );
