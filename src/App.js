9b07ba09f3cb36a2904b9c53672cbfa93ebf3533
Karl Umberto <karl.pongolini@gmail.com>
2025-04-04 13:33:55 +0300

    Leaflet API integration, added markers, added marker clusters and a bit CSS (design)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
 import './App.css';
-import { MapContainer } from 'react-leaflet/MapContainer'
+import { MapContainer, Marker } from 'react-leaflet'
 import { TileLayer } from 'react-leaflet/TileLayer'
-import { useMap } from 'react-leaflet/hooks'
+import { Icon } from 'leaflet';
+import { Popup } from 'react-leaflet';
+import MarkerClusterGroup from "react-leaflet-markercluster";
 import "leaflet/dist/leaflet.css";
 
 
 function App() {
+
+const markers = [
+{
+  geocode: [59.443475262102396, 24.79419282429169],
+  name: "Russalka",
+  description: "See on Russalkas",
+  link: "",
+},
+{  geocode: [59.443475262102396, 24.79419282429169],
+  name: "Russalka2",
+  description: "See on Russalkas",
+  link: "",}
+];
+
+
+const customIcon = new Icon({
+  iconUrl: require("./markerkarl.png"),
+  iconSize: [38,38]
+})
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <div id="box">
-      <MapContainer center={[58.2667, 25.5844]} zoom={13} scrollWheelZoom={false} style={{ height: "50%", width: "50%" }}>
+      <MapContainer center={[59.443475262102396, 24.79419282429169]} zoom={13} scrollWheelZoom={false} style={{ height: "100%", width: "100%" }}>
       <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
+
+    <MarkerClusterGroup>
+
+{markers.map((marker) => (
+  <Marker position={marker.geocode} icon={customIcon}>
+  <Popup>{marker.popUp}</Popup>
+</Marker>
+))}
+</MarkerClusterGroup>
 </MapContainer>
 </div>
       </header>
