afbab96f9764f223b8afa61f8d4c075284afca43
Karl Umberto <karl.pongolini@gmail.com>
2025-04-04 16:54:37 +0300

    Added loc json, trying to add markers from it to the map

--- src/App.js
+++ src/App.js
@@ -5,11 +5,66 @@ import { Icon } from 'leaflet';
 import { Popup } from 'react-leaflet';
 import MarkerClusterGroup from "react-leaflet-markercluster";
 import "leaflet/dist/leaflet.css";
+import { useState, useEffect} from 'react';
 
+const MarkerInfo = ({ marker}) => {
+  const { name, address, phone, openingHours, website, wheelchairAccessible, coordinates} = marker;
+
+  
+  return (
+    <div style={{ marginBottom: '20px', border: '1px solid #ccc', padding: '10px' }}>
+      <h2>{name}</h2>
+      <p><strong>Address:</strong> {address}</p>
+      <p><strong>Phone:</strong> {phone}</p>
+      <p><strong>Opening Hours:</strong> {openingHours}</p>
+      <p><strong>Website:</strong> <a href={website} target="_blank" rel="noopener noreferrer">{website}</a></p>
+      <p><strong>Wheelchair Accessible:</strong> {wheelchairAccessible ? 'Yes' : 'No'}</p>
+      <p><strong>Coordinates:</strong> Latitude: {coordinates[1]}, Longitude: {coordinates[0]}</p>
+    </div>
+  );
+};
 
 function App() {
 
-const markers = [
+  const [markers, setMarkers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  console.log("laura");
+  useEffect(() =>{
+    fetch('/geolocations.json')
+    .then((response) => response.json())
+    .then((data) =>{
+      console.log(data);
+      const markerList = data.features.map((item) => {
+        const { properties, geometry} = item;
+        console.log(item);
+        const address = `${properties['addr:street']} ${properties['addr:housenumber']}, ${properties['addr:country']} ${properties['addr:postcode']}`;
+        const coordinates = geometry.coordinates[0][0];
+
+        return{ 
+          name: properties.name,
+          address,
+          phone: properties.phone,
+          openingHours: properties.opening_hours,
+          website: properties.website,
+          wheelchairAccessible: properties.wheelchair === 'yes',
+          coordinates: [coordinates[1], coordinates[0]],
+        };
+      });
+      setMarkers(markerList);
+      setLoading(false);
+    })
+    .catch((error) => {
+      console.log('error', error);
+      setLoading(false);
+    });
+  }, []);
+
+
+
+
+
+const markers2 = [
 {
   geocode: [59.443475262102396, 24.79419282429169],
   name: "Russalka",
@@ -39,20 +94,22 @@ const customIcon = new Icon({
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-
     <MarkerClusterGroup>
-
-{markers.map((marker) => (
-  <Marker position={marker.geocode} icon={customIcon}>
-  <Popup>{marker.popUp}</Popup>
-</Marker>
-))}
-</MarkerClusterGroup>
-</MapContainer>
+      {markers.map((marker, index) => (
+        <Marker key={index} position={marker.coordinates} icon={customIcon}>
+          <Popup>
+            <MarkerInfo marker={marker} />
+          </Popup>
+        </Marker>
+        ))}
+    </MarkerClusterGroup>
+  </MapContainer>
 </div>
       </header>
     </div>
   );
 }
+
+
 
 export default App;
